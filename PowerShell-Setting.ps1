1f8b4f3a750e521cb90b99a448bc54e689fc6b1e
Hazmi Hashim <MuhdHazmiAdli.MohdHashim@sony.com>
2025-01-17 17:03:58 +0800

    Adding PS Script File

---
+++ PowerShell-Setting.ps1
@@ -0,0 +1,6 @@
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/takuya.omp.json" | Invoke-Expression
+
+Import-Module -Name Terminal-Icons
+
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
