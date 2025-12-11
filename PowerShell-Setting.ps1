871ff3fd3ccde0448340659248d94621567c769c
Hazmi Hashim <97172931+HARDZMY@users.noreply.github.com>
2025-12-11 15:37:42 +0800

    Update PowerShell-Setting.ps1

--- PowerShell-Setting.ps1
+++ PowerShell-Setting.ps1
@@ -1,6 +1,12 @@
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/takuya.omp.json" | Invoke-Expression
+# Environment path to themes folder
+$env:POSH_THEMES_PATH = "$HOME\OneDrive\Documents\PowerShell\themes"
 
+# Init Oh My Posh with Takuya theme
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\takuya.omp.json" | Invoke-Expression
+
+# Terminal Icon Module
 Import-Module -Name Terminal-Icons
 
+# PSReadLine Module
 Set-PSReadLineOption -PredictionSource History
 Set-PSReadLineOption -PredictionViewStyle ListView
